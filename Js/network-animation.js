72304ff69daf73b492fc009b6dc7688aace8599a
Borba <borbanicolasmk@gmail.com>
2022-04-15 02:58:40 -0300

    working on buttons

--- Js/network-animation.js
+++ Js/network-animation.js
@@ -3,7 +3,7 @@ var w, h, loopId, id, canvas, ctx, particles;
 var options = {
     particleColor: "rgba(255,255,255)",
     lineColor: "rgb(72,130,134)",
-    particleAmount: 40,
+    particleAmount: 25,
     defaultRadius: 2,
     variantRadius: 1,
     defaultSpeed: 1,
